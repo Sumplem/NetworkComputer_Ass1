f78ccb939641c57742e5bf4425875946a30d1c00
Sumplem <lucgiahung310@gmail.com>
2023-10-11 17:18:01 +0700

    fix

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 define('_HOST','localhost');
 define('_USER_NAME','root');
 define('_PASSWORD','');
-define('_DATABASE','vnex');
-define('_DIR','VNEX');
+define('_DATABASE','chatchit');
+define('_DIR','NetworkComputer_Ass1');
 
 $root = 'http://'.$_SERVER['HTTP_HOST'].'/'._DIR;
 define('_WEB_ROOT',$root);
